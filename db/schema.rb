b8d8e68e647fd1f84bec3f542c20ba105b87a6d1
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-22 20:17:52 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2019_09_22_190704) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "release_date"
-    t.string "director"
-    t.string "lead"
-    t.boolean "in_theaters"
-  end
-
 end
