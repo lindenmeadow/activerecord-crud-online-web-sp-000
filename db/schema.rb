32f4d6a042868e26c3d599fe540133d076c87982
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-22 20:21:55 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_190704) do
+ActiveRecord::Schema.define(version: 2019_09_22_202016) do
+
+  create_table "create_movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
 
 end
