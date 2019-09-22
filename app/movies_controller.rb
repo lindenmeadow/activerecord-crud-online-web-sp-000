45c40b85eafcb27bf812f56a0ced558b2eef1e66
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-22 20:55:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "Dark Waters"
-    m.release_date = 2005
+    m.title
+    m.release_date
   end
 end
 
